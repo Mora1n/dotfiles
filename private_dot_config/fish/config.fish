a58715620bfde21a0ec871f710ebe0896cb4f4b4
morain <morain.fu@gmail.com>
2025-09-23 16:09:56 +0800

    feat: add fish config

---
+++ private_dot_config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+
+    # eza aliases
+    alias l='eza'
+    alias la='eza -a'
+    alias ll='eza -l'
+
+end
