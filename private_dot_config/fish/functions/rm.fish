22ae14eda3f7840b3ed8db3b96451ff1ca08e57d
morain <morain.fu@gmail.com>
2025-10-23 23:30:48 +0800

    feat: add safe rm

---
+++ private_dot_config/fish/functions/rm.fish
@@ -0,0 +1,10 @@
+# Prevent accidental deletion of root directory
+function rm
+    for arg in $argv
+        if string match -qr '^-{0,2}(r|rf|fr)' $arg; and string match -q '/' $argv
+            echo "Error: Deletion of root directory is prohibited" >&2
+            return 1
+        end
+    end
+    command rm $argv
+end
